91615358b5b3dfd24e0dba1533c0c9a416cec079
ADMIN <hoang>
2021-05-24 11:07:31 +0700

    fix blog page

--- js/index.js
+++ js/index.js
@@ -40,8 +40,6 @@ $(document).ready(function () {
 
   $(function () {
     $.validator.addMethod('regex', function (value, element, param) {
-        console.log(param);
-        console.log(value);
         return this.optional(element) ||
           value.match(typeof param == 'string' ? new RegExp(param) : param);
       },
@@ -62,7 +60,6 @@ $(document).ready(function () {
       },
       submitHandler: function (form) {
         form.submit();
-        console.log("submit");
       }
     });
 
@@ -95,7 +92,6 @@ $(document).ready(function () {
       },
       submitHandler: function (form) {
         form.submit();
-        console.log("submit");
       },
     });
   });
@@ -104,7 +100,6 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".dropdown-item").click(function () {
     $(".dropdown-item").val("1");
-    console.log($(".dropdown-item").value);
   });
 });
 
@@ -150,8 +145,3 @@ $(function () {
     variableWidth: true,
   });
 });
-
-// $(function () {
-//   console.log($('.blog-slide .slick-dots li').width());
-//   console.log($('.slick-dots li').length);
-// })
